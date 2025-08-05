dc2abe747b1ba3787a574c97398c645e01722abd
SamWaku <samuelwaku1st@gmail.com>
2025-08-05 17:06:24 +0200

    chore: Welcom page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:apploans/pages/welcome/welcome.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'app_blocs.dart';
 
@@ -16,10 +17,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => AppBlocs(),
-      child: const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Welcome(),
-      ),
+      child:  ScreenUtilInit(
+        builder: (context, child) => MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: Welcome(),
+        ),
+      )
     );
   }
 }
