6a84b6fc39e9d5703218edcf1499dd387a8ac952
SamWaku <samuelwaku1st@gmail.com>
2025-08-06 17:31:16 +0200

    chore: welcome screen 1

--- lib/pages/welcome/welcome.dart
+++ lib/pages/welcome/welcome.dart
@@ -12,25 +12,64 @@ class Welcome extends StatefulWidget {
 class _WelcomeState extends State<Welcome> {
   @override
   Widget build(BuildContext context) {
-    return  Container(
+    return Container(
       color: Colors.white,
       child: Scaffold(
         body: Container(
+          margin: EdgeInsets.only(top: 34.h),
           width: 375.w,
           child: Stack(
             alignment: Alignment.topCenter,
             children: [
               PageView(
                 children: [
-                  SizedBox(
-                    width: 345.w,
-                    height: 345.w,
-                    child: Text(
-                      "Image One"
-                    ),
-                  )
+                  Column(
+                    children: [
+                      SizedBox(
+                        width: 345.w,
+                        height: 345.w,
+                        child: Text("Image One"),
+                      ),
+                      Container(
+                        child: Text(
+                          "Need a loan?",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 24.sp,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 375.w,
+                        padding: EdgeInsets.only(left: 30.w, right: 30.w),
+                        child: Text(
+                          "Why stress about your financial needs. Get a loan with us.",
+                          style: TextStyle(
+                            color: Colors.black.withOpacity(0.5),
+                            fontSize: 14.sp,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 100.h, left: 25.w, right: 25.w),
+                        width: 375.w,
+                        height: 50.h,
+                        decoration: BoxDecoration(
+                          color: Colors.blueAccent,
+                          borderRadius: BorderRadius.all(Radius.circular(15.w)),
+                          boxShadow: [
+                            BoxShadow(
+                              colo
+                            )
+                          ]
+                        ),
+                      )
+                    ],
+                  ),
                 ],
-              )
+              ),
             ],
           ),
         ),
