10cf238f803e15439ac6207476fc2a18f64142c0
SamWaku <samuelwaku1st@gmail.com>
2025-08-06 17:41:13 +0200

    chore: welcome screen button resuable

--- lib/pages/welcome/welcome.dart
+++ lib/pages/welcome/welcome.dart
@@ -23,74 +23,82 @@ class _WelcomeState extends State<Welcome> {
             children: [
               PageView(
                 children: [
-                  Column(
-                    children: [
-                      SizedBox(
-                        width: 345.w,
-                        height: 345.w,
-                        child: Text("Image One"),
-                      ),
-                      Container(
-                        child: Text(
-                          "Need a loan?",
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 24.sp,
-                            fontWeight: FontWeight.normal,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        width: 375.w,
-                        padding: EdgeInsets.only(left: 30.w, right: 30.w),
-                        child: Text(
-                          "Why stress about your financial needs. Get a loan with us.",
-                          style: TextStyle(
-                            color: Colors.black.withOpacity(0.5),
-                            fontSize: 14.sp,
-                            fontWeight: FontWeight.normal,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(
-                          top: 100.h,
-                          left: 25.w,
-                          right: 25.w,
-                        ),
-                        width: 375.w,
-                        height: 50.h,
-                        decoration: BoxDecoration(
-                          color: Colors.blueAccent,
-                          borderRadius: BorderRadius.all(Radius.circular(15.w)),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.1),
-                              spreadRadius: 1,
-                              blurRadius: 2,
-                              offset: Offset(0, 1),
-                            ),
-                          ],
-                        ),
-                        child: Center(
-                          child: Text(
-                            "next",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 16.sp,
-                                fontWeight: FontWeight.normal
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                  _page()
                 ],
               ),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget _page(){
+    return Column(
+      children: [
+        Column(
+          children: [
+            SizedBox(
+              width: 345.w,
+              height: 345.w,
+              child: Text("Image One"),
+            ),
+            Container(
+              child: Text(
+                "Need a loan?",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 24.sp,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+            ),
+            Container(
+              width: 375.w,
+              padding: EdgeInsets.only(left: 30.w, right: 30.w),
+              child: Text(
+                "Why stress about your financial needs. Get a loan with us.",
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.5),
+                  fontSize: 14.sp,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(
+                top: 100.h,
+                left: 25.w,
+                right: 25.w,
+              ),
+              width: 375.w,
+              height: 50.h,
+              decoration: BoxDecoration(
+                color: Colors.blueAccent,
+                borderRadius: BorderRadius.all(Radius.circular(15.w)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.1),
+                    spreadRadius: 1,
+                    blurRadius: 2,
+                    offset: Offset(0, 1),
+                  ),
+                ],
+              ),
+              child: Center(
+                child: Text(
+                  "next",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.sp,
+                      fontWeight: FontWeight.normal
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
