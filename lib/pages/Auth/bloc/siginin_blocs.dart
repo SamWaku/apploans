613df77f2c3957c1e3e6b468246696771f04e6d3
SamWaku <samuelwaku1st@gmail.com>
2025-09-12 16:10:27 +0200

    chore: email events

--- lib/pages/Auth/bloc/siginin_blocs.dart
+++ lib/pages/Auth/bloc/siginin_blocs.dart
@@ -4,16 +4,16 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SignInBloc extends Bloc<SignInEvent, SignInState> {
   SignInBloc() :super(const SignInState()) {
-    on<EmailEvent>((event, emit) {
-      emit(state.copyWith(email: event.email));
-    });
+    on<EmailEvent>(_emailEvent);
 
-    on<PasswordEvent>((event, emit){
-      emit(state.copyWith(password: event.password));
-    });
+    on<PasswordEvent>(_passwordEvent);
   }
 
   void _emailEvent(EmailEvent event, Emitter<SignInState> emit){
     emit(state.copyWith(email: event.email));
+  }
+
+  void _passwordEvent(PasswordEvent event, Emitter<SignInState> emit){
+    emit(state.copyWith(password: event.password));
   }
 }
