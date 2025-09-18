44f939d0a5832cc07761033191d35c47ccd484a5
SamWaku <samuelwaku1st@gmail.com>
2025-09-18 18:56:15 +0200

    chore: signup states

--- lib/pages/Auth/sign_up/bloc/signup_states.dart
+++ lib/pages/Auth/sign_up/bloc/signup_states.dart
@@ -1,10 +1,28 @@
 //make it immutable to prevent the class from being changed(const and final modifiers)
-class SignUpStates{
+class SignUpStates {
   final String userName;
   final String email;
   final String password;
   final String confirmPassword;
 
-  const SignUpStates(this.userName, this.email, this.password, this.confirmPassword);
+  const SignUpStates(
+    this.userName,
+    this.email,
+    this.password,
+    this.confirmPassword,
+  );
 
+  SignUpStates copyWith({
+    String? userName,
+    String? email,
+    String? password,
+    String? confirmPassword,
+  }) {
+    return SignUpStates(
+      userName ?? this.userName,
+      email ?? this.email,
+      password ?? this.password,
+      confirmPassword ?? this.confirmPassword,
+    );
+  }
 }
