abe34d5155d02354e20b48752b731dce1e8e4347
SamWaku <samuelwaku1st@gmail.com>
2025-09-18 22:06:28 +0200

    chore: signup controller null checks

--- lib/pages/Auth/sign_up/sign_up_controller.dart
+++ lib/pages/Auth/sign_up/sign_up_controller.dart
@@ -1,5 +1,6 @@
 import 'package:apploans/pages/Auth/sign_up/bloc/signup_blocs.dart';
 import 'package:apploans/pages/Auth/sign_up/bloc/signup_states.dart';
+import 'package:apploans/utils/flutter_toast.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -9,5 +10,13 @@ class SignUpController{
 
  void handleEmailSignUp(){
    var state = context.read<SignUpBlocs>().state;
+   String username = state.userName;
+   String email = state.email;
+   String password = state.password;
+   String confirmPassword = state.confirmPassword;
+
+   if(username.isEmpty){
+     toastInfo(context: context, msg: "User name is empty");
+   }
  }
 }
