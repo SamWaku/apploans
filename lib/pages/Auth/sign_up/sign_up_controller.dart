9d82275c726507a32c00f4e45b55554327e7e616
SamWaku <samuelwaku1st@gmail.com>
2025-09-18 22:03:24 +0200

    chore: signup controller states

--- lib/pages/Auth/sign_up/sign_up_controller.dart
+++ lib/pages/Auth/sign_up/sign_up_controller.dart
@@ -1,3 +1,13 @@
-class SignUpController{
+import 'package:apploans/pages/Auth/sign_up/bloc/signup_blocs.dart';
+import 'package:apploans/pages/Auth/sign_up/bloc/signup_states.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+class SignUpController{
+ final BuildContext context;
+ const SignUpController(this.context);
+
+ void handleEmailSignUp(){
+   var state = context.read<SignUpBlocs>().state;
+ }
 }
