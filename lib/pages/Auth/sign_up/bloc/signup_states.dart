2afada7ae157e298b4e0a8e32c8f28d14823654b
SamWaku <samuelwaku1st@gmail.com>
2025-09-18 18:43:13 +0200

    chore: Signup states

---
+++ lib/pages/Auth/sign_up/bloc/signup_states.dart
@@ -0,0 +1,8 @@
+class SignUpStates{
+  String userName;
+  String email;
+  String password;
+  String confirmPassword;
+
+  SignUpStates(this.userName, this.email, this.password, this.confirmPassword);
+}
