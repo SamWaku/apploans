b8df5e769b7cd84161623826b38375cce44d3df9
SamWaku <samuelwaku1st@gmail.com>
2025-09-18 19:27:01 +0200

    chore: passed register blocs

--- lib/pages/Auth/sign_up/sign_up.dart
+++ lib/pages/Auth/sign_up/sign_up.dart
@@ -1,5 +1,6 @@
 import 'package:apploans/pages/Auth/sign_in/widgets/sign_in_widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SignUp extends StatefulWidget {
@@ -12,56 +13,58 @@ class SignUp extends StatefulWidget {
 class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildAppBar("Sign Up"),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(child: reusableText("Create your account ")),
-            Container(
-              margin: EdgeInsets.only(top: 26.h),
-              padding: EdgeInsets.only(left: 25.h),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  reusableText("Username"),
-                  buildTextField("username", "Username", "user", "Enter your username",
-                          (value){
+    return BlocBuilder(builder: (context, state){
+      return Scaffold(
+        appBar: buildAppBar("Sign Up"),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(child: reusableText("Create your account ")),
+              Container(
+                margin: EdgeInsets.only(top: 26.h),
+                padding: EdgeInsets.only(left: 25.h),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    reusableText("Username"),
+                    buildTextField("username", "Username", "user", "Enter your username",
+                            (value){
 
-                      }
-                  ),
-                  reusableText("Email"),
-                  buildTextField("email", "Email", "user", "Enter your email",
-                          (value){
+                        }
+                    ),
+                    reusableText("Email"),
+                    buildTextField("email", "Email", "user", "Enter your email",
+                            (value){
 
-                      }
-                  ),
-                  reusableText("Password"),
-                  buildTextField("password", "Password", "lock", "Enter your password",
-                          (value){
+                        }
+                    ),
+                    reusableText("Password"),
+                    buildTextField("password", "Password", "lock", "Enter your password",
+                            (value){
 
-                      }
-                  ),
-                  reusableText("Confirm Password"),
-                  buildTextField("password", "Password", "lock", "Confirm your password",
-                          (value){
+                        }
+                    ),
+                    reusableText("Confirm Password"),
+                    buildTextField("password", "Password", "lock", "Confirm your password",
+                            (value){
 
-                      }),
+                        }),
 
-                  buildLoginRegButton("Sign Up", "Register", (){
+                    buildLoginRegButton("Sign Up", "Register", (){
 
-                  }),
-                ],
+                    }),
+                  ],
+                ),
               ),
-            ),
-            Container(
-                margin: EdgeInsets.only(top: 10.h),
-                child: Center(child: reusableText("or sign up with any"))),
-            buildThirdPartyLogin(context),
-          ],
+              Container(
+                  margin: EdgeInsets.only(top: 10.h),
+                  child: Center(child: reusableText("or sign up with any"))),
+              buildThirdPartyLogin(context),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
