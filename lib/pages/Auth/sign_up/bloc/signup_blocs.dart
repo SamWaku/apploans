cd1f9968282acc270f36c373bcd25e7771ee17dc
SamWaku <samuelwaku1st@gmail.com>
2025-09-18 19:06:27 +0200

    chore: signup states

---
+++ lib/pages/Auth/sign_up/bloc/signup_blocs.dart
@@ -0,0 +1,9 @@
+import 'package:apploans/pages/Auth/sign_up/bloc/signup_events.dart';
+import 'package:apploans/pages/Auth/sign_up/bloc/signup_states.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SignUpBlocs extends Bloc<SignUpEvent, SignUpStates>{
+  SignUpBlocs() : super (SignUpStates(){
+
+  });
+}
