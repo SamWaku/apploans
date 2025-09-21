0544bc507e76d7005c55c880c7f4147af3e73f8e
SamWaku <samuelwaku1st@gmail.com>
2025-09-21 13:13:59 +0200

    chore: home and search

--- lib/pages/Home/home_page.dart
+++ lib/pages/Home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../utils/application_widgets.dart';
 
@@ -9,12 +10,26 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          title: Text("Home")
-        ),
-        body: Column(
-          children: [
-            buildPage(1)
+        appBar: AppBar(title: Text("Home")),
+        body: Column(children: [buildPage(1)]),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              label: "Home",
+              icon: SizedBox(
+                width: 15.w,
+                height: 15.h,
+                child: Image.asset("assets/icons/home.png"),
+              )
+          ),
+            BottomNavigationBarItem(
+                label: "Discover",
+                icon: SizedBox(
+                  width: 15.w,
+                  height: 15.h,
+                  child: Image.asset("assets/icons/search.png"),
+                )
+            ),
           ],
         ),
       ),
