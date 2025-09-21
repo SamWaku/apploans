3c7a53df2b32d90048736a608f20087b45aa3383
SamWaku <samuelwaku1st@gmail.com>
2025-09-21 12:58:55 +0200

    chore: build page

---
+++ lib/utils/application_widgets.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+Widget buildPage(int index){
+  List<Widget> _widget = [
+    Center(child: Text("Top Most Section")),
+    Center(child: Text("Middle Section")),
+    Center(child: Text("Bottom Section")),
+    Center(child: Text("Top Section")),
+    Center(child: Text("Top Section")),
+  ];
+}
