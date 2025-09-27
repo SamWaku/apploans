dcebf02d1b8dc39587468bfa3d64096debbf96fa
SamWaku <samuelwaku1st@gmail.com>
2025-09-27 11:21:11 +0200

    chore: app pages

--- lib/common/routes/pages.dart
+++ lib/common/routes/pages.dart
@@ -1,8 +1,25 @@
-//unify blocprovider routes and pages
+// //unify blocprovider routes and pages
+import 'package:apploans/common/routes/routes.dart';
+import 'package:apploans/pages/welcome/bloc/welcome_blocs.dart';
+import 'package:apploans/pages/welcome/welcome.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AppPages{
+  List<PageEntity> routes = [
+    PageEntity(
+        routes: AppRoutes.INITIAL,
+        page: Welcome(),
+        bloc: BlocProvider(create: (_) => WelcomeBloc())
+    )
+  ];
+}
+
 class PageEntity{
   String routes;
   Widget page;
   dynamic bloc;
 
-
+  PageEntity({required this.routes, required this.page, required this.bloc});
 }
+
