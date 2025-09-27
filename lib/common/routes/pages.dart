1b54286ab3714e27cbe7b5fa29a374b61c49f987
SamWaku <samuelwaku1st@gmail.com>
2025-09-27 11:54:20 +0200

    chore: app pages

--- lib/common/routes/pages.dart
+++ lib/common/routes/pages.dart
@@ -1,19 +1,51 @@
 // //unify blocprovider routes and pages
 import 'package:apploans/common/routes/routes.dart';
+import 'package:apploans/pages/Auth/sign_in/sign_in.dart';
+import 'package:apploans/pages/Auth/sign_up/bloc/signup_blocs.dart';
+import 'package:apploans/pages/Auth/sign_up/sign_up.dart';
 import 'package:apploans/pages/welcome/bloc/welcome_blocs.dart';
 import 'package:apploans/pages/welcome/welcome.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppPages{
-  List<PageEntity> routes = [
-    PageEntity(
-        routes: AppRoutes.INITIAL,
-        page: Welcome(),
-        bloc: BlocProvider(create: (_) => WelcomeBloc())
-    )
-  ];
+  static List<PageEntity> routes() {
+    return [
+      PageEntity(
+          routes: AppRoutes.INITIAL,
+          page: Welcome(),
+          bloc: BlocProvider(create: (_) => WelcomeBloc())
+      ),
+      PageEntity(
+          routes: AppRoutes.SIGN_IN,
+          page: SignIn(),
+          bloc: BlocProvider(create: (_) => SignUpBlocs(),)
+      ),
+      PageEntity(
+          routes: AppRoutes.SIGN_UP,
+          page: SignUp(),
+          bloc: BlocProvider(create: (_) => SignUpBlocs())
+      ),
+      PageEntity(
+          routes: AppRoutes.HOMEPAGE,
+          page: Welcome(),
+          bloc: BlocProvider(create: (_) => SignUpBlocs())
+      )
+    ];
+
+  }
+
+  static List<dynamic> allBlocProviders(BuildContext context){
+    List<dynamic> blocProviders = <dynamic>[];
+    for(var bloc in routes()){
+      blocProviders.add(bloc.bloc)
+    }
+  }
 }
+
+
+
+
 
 class PageEntity{
   String routes;
