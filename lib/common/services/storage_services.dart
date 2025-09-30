147e3794b3ad3297ca03429190753ab8098a9caa
SamWaku <samuelwaku1st@gmail.com>
2025-09-30 15:31:06 +0200

    choreL storage services

--- lib/common/services/storage_services.dart
+++ lib/common/services/storage_services.dart
@@ -1,3 +1,10 @@
-class StorageServices{
+import 'package:shared_preferences/shared_preferences.dart';
 
+class StorageServices{
+  late final SharedPreferences _prefs
+;
+  Future<StorageServices> init() async {
+    _prefs = await SharedPreferences.getInstance();
+    return this;
+  }
 }
