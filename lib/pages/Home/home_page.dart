57270720c729af6471c9eb5cac941e3c055f03e6
SamWaku <samuelwaku1st@gmail.com>
2025-10-02 17:51:50 +0200

    chore: setup appbar

--- lib/pages/Home/home_page.dart
+++ lib/pages/Home/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:apploans/pages/Home/widgets/home_page_wigets.dart';
 import 'package:flutter/material.dart';
+
+import '../Auth/sign_in/widgets/sign_in_widgets.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,8 +13,8 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-
-    );
+    return Scaffold(
+      appBar: homeBuildAppBar,
+    )
   }
 }
---
+++ lib/pages/Home/widgets/home_page_wigets.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+AppBar homeBuildAppBar(){
+  return AppBar(
+    title: Container(
+      child: Row(
+        children: [
+
+        ],
+      ),
+    ),
+  );
+}
