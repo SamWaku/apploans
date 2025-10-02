d68dbdafec7daa3e81a9778c68a6130db42d731f
SamWaku <samuelwaku1st@gmail.com>
2025-10-02 17:54:58 +0200

    chore: app bar menu

--- lib/pages/Home/home_page.dart
+++ lib/pages/Home/home_page.dart
@@ -14,7 +14,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: homeBuildAppBar,
-    )
+      appBar: homeBuildAppBar(),
+    );
   }
 }
--- lib/pages/Home/widgets/home_page_wigets.dart
+++ lib/pages/Home/widgets/home_page_wigets.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 AppBar homeBuildAppBar(){
   return AppBar(
     title: Container(
       child: Row(
         children: [
-
+          SizedBox(
+            width: 15.w,
+            height: 12.h,
+            child: Image.asset("assets/icons/menu.png"),
+          )
         ],
       ),
     ),
