2c49e870559bc4ff2cecf1cfc8f3e528a75f3307
SamWaku <samuelwaku1st@gmail.com>
2025-10-11 07:50:06 +0200

    chore: homepage dots

--- lib/pages/Home/bloc/home_page_blocs.dart
+++ lib/pages/Home/bloc/home_page_blocs.dart
@@ -3,5 +3,11 @@ import 'package:apploans/pages/Home/bloc/home_page_states.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePageBlocs extends Bloc<HomePageEvents, HomePageStates>{
-  HomePageBlocs() : super (HomePageStates());
+  HomePageBlocs() : super (HomePageStates()){
+    on<HomePageDots>(_homePageDots);
+  }
+
+  void _homePageDots(HomePageDots, Emitter<HomePageStates> emit){
+
+  }
 }
