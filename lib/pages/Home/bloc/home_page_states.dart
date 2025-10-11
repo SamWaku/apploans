3b2e1c4e891006b011ad8f5f136204d894da64d8
SamWaku <samuelwaku1st@gmail.com>
2025-10-11 07:43:57 +0200

    chore: homepage events

--- lib/pages/Home/bloc/home_page_states.dart
+++ lib/pages/Home/bloc/home_page_states.dart
@@ -2,7 +2,7 @@ class HomePageStates{
   const HomePageStates({this.index = 0});
 
   final int index;
-  HomePageStates copyWith(int index){
+  HomePageStates copyWith({int? index}){
     return HomePageStates(
       index: index ?? this.index
     );
