6cd00a3f12c3517c0e5a8882c839dacd6bb5a484
SamWaku <samuelwaku1st@gmail.com>
2025-10-11 07:47:32 +0200

    chore: setup bloc state

---
+++ lib/pages/Home/bloc/home_page_blocs.dart
@@ -0,0 +1,7 @@
+import 'package:apploans/pages/Home/bloc/home_page_events.dart';
+import 'package:apploans/pages/Home/bloc/home_page_states.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class HomePageBlocs extends Bloc<HomePageEvents, HomePageStates>{
+  HomePageBlocs() : super (HomePageStates());
+}
