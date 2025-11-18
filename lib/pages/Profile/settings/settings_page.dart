be372bdaf6f128c97a7578e240c21f796e8246e1
SamWaku <samuelwaku1st@gmail.com>
2025-11-18 15:10:08 +0200

    settings

--- lib/pages/Profile/settings/settings_page.dart
+++ lib/pages/Profile/settings/settings_page.dart
@@ -4,6 +4,7 @@ import 'package:apploans/pages/Profile/settings/bloc/settings_states.dart';
 import 'package:apploans/pages/Profile/settings/widgets/settings_widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
@@ -16,11 +17,26 @@ class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: SettingsAppBar(),
       body: SingleChildScrollView(
         child: BlocBuilder<SettingsPageBlocs, SettingsPageStates>(
           builder: (context, state) {
-            return Container(child: Column(children: [Text("Settings")]));
+            return
+              Container
+                (child: Column
+                (
+                  children:
+                  [
+                    Container(
+                      height: 100.w,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(image: AssetImage("assets/icons/Logout.png"))
+                      ),
+                    )
+                  ]
+                )
+              );
           },
         ),
       ),
--- lib/pages/Profile/settings/widgets/settings_widgets.dart
+++ lib/pages/Profile/settings/widgets/settings_widgets.dart
@@ -4,16 +4,17 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 AppBar SettingsAppBar() {
   return AppBar(
+    backgroundColor: Colors.white,
     title: Container(
       padding: EdgeInsets.only(left: 85.w),
-          child: Text(
-            "Settings",
-            style: TextStyle(
-              color: AppColors.primaryText,
-              fontWeight: FontWeight.bold,
-              fontSize: 16.sp,
-            ),
-          ),
-        )
-    );
+      child: Text(
+        "Settings",
+        style: TextStyle(
+          color: AppColors.primaryText,
+          fontWeight: FontWeight.normal,
+          fontSize: 16.sp,
+        ),
+      ),
+    ),
+  );
 }
