4dc6e2f364ffa7baaca3ad33c8e02b9d96efb94f
SamWaku <samuelwaku1st@gmail.com>
2025-11-18 15:03:46 +0200

    core: appbar padding

--- lib/pages/Profile/settings/widgets/settings_widgets.dart
+++ lib/pages/Profile/settings/widgets/settings_widgets.dart
@@ -1,5 +1,19 @@
+import 'package:apploans/common/values/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-AppBar SettingsAppBar(){
-  return AppBar();
+AppBar SettingsAppBar() {
+  return AppBar(
+    title: Container(
+      padding: EdgeInsets.only(left: 85.w),
+          child: Text(
+            "Settings",
+            style: TextStyle(
+              color: AppColors.primaryText,
+              fontWeight: FontWeight.bold,
+              fontSize: 16.sp,
+            ),
+          ),
+        )
+    );
 }
