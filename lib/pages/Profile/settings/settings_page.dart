6e969dc22d536075172e01c4cc47c0d73d9393cb
SamWaku <samuelwaku1st@gmail.com>
2026-01-04 14:45:41 +0200

    core: triggers home events to index zero

--- lib/pages/Profile/settings/settings_page.dart
+++ lib/pages/Profile/settings/settings_page.dart
@@ -1,5 +1,7 @@
 import 'package:apploans/common/routes/names.dart';
 import 'package:apploans/common/values/constants.dart';
+import 'package:apploans/pages/Application/bloc/application_blocs.dart';
+import 'package:apploans/pages/Application/bloc/application_events.dart';
 import 'package:apploans/pages/Profile/settings/bloc/settings_blocs.dart';
 import 'package:apploans/pages/Profile/settings/bloc/settings_states.dart';
 import 'package:apploans/pages/Profile/settings/widgets/settings_widgets.dart';
@@ -12,14 +14,13 @@ import '../../../global.dart';
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
 
-
-
   @override
   State<SettingsPage> createState() => _SettingsPageState();
 }
 
 class _SettingsPageState extends State<SettingsPage> {
   void logout(){
+    context.read<ApplicationBlocs>().add(TriggerHomeEvent(0));
     Global.storageService.remove(
         AppConstants.STORAGE_USER_TOKEN_KEY
     );
