8367c99b67fee2755207c03ffd13012dec98a81e
SamWaku <samuelwaku1st@gmail.com>
2026-02-10 05:28:38 +0200

    core: setup dio

--- lib/common/utils/http_util.dart
+++ lib/common/utils/http_util.dart
@@ -1,11 +1,20 @@
 //singleton class that will only exist once in a lifetime
+import 'package:dio/dio.dart';
+
 class HttpUtil{
   static HttpUtil _intstance = HttpUtil._internal();
   factory HttpUtil(){
     return _intstance;
   }
 
+  late Dio dio;
   HttpUtil._internal(){
-
+    BaseOptions options = BaseOptions(
+      baseUrl: "http://127.0.0.1:5041",
+      connectTimeout: Duration(seconds: 5),
+      receiveTimeout: Duration(seconds: 5),
+      headers: {},
+      contentType: "application/json",
+    );
   }
 }
