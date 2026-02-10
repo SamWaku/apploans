703924d77886b088b1c59ffeced61e93bdb32ec1
SamWaku <samuelwaku1st@gmail.com>
2026-02-10 05:09:45 +0200

    core:user entity to json

--- lib/common/entities/user.dart
+++ lib/common/entities/user.dart
@@ -3,10 +3,20 @@ class LoginRequestEntity{
   String? name;
   String? description;
   String? email;
-  String? phonr;
+  String? phone;
   String? avatar;
   String? open_id;
   int? online;
 
-  LoginRequestEntity({this.type, this.name, this.description, this.email, this.phonr, this.avatar, this.open_id});
+  LoginRequestEntity({this.type, this.name, this.description, this.email, this.phone, this.avatar, this.open_id});
+
+  Map<String, dynamic> toJson() => {
+    "type": type,
+    "name": name,
+    "description": description,
+    "email": email,
+    "phone": phone,
+    "avatar": avatar,
+    "open_id": open_id
+  };
 }
