5f2e884e321d6aa49579ce1c5e295bb9915b3c6b
SamWaku <samuelwaku1st@gmail.com>
2026-06-09 06:30:25 +0200

    core: signing method

--- lib/pages/Auth/sign_up/sign_up_controller.dart
+++ lib/pages/Auth/sign_up/sign_up_controller.dart
@@ -66,4 +66,33 @@ class SignUpController {
       }
     }
   }
+
+  Future<void> handleClientRegistration() async{
+    var state = context.read<SignUpBlocs>().state;
+    String username = state.userName;
+    String email = state.email;
+    String password = state.password;
+    String confirmPassword = state.confirmPassword;
+
+    if (username.isEmpty) {
+      toastInfo(context: context, msg: "User name is empty");
+      return;
+    }
+
+    if (email.isEmpty) {
+      toastInfo(context: context, msg: "Email is empty");
+      return;
+    }
+
+    if (password.isEmpty) {
+      toastInfo(context: context, msg: "Password is empty");
+      return;
+    }
+
+    if (confirmPassword.isEmpty) {
+      toastInfo(context: context, msg: "Please confirm you password");
+      return;
+    }
+
+  }
 }
